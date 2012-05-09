aac0ca9f3f52de48309a4a6bd51a617913aa5699
Burt Beckwith <beckwithb@vmware.com>
2012-05-09 11:35:06 -0400

    cleanup; made http and apikey fields private since the service is a shared singleton

--- grails-app/services/co/happyinc/MailchimpSTSService.groovy
+++ grails-app/services/co/happyinc/MailchimpSTSService.groovy
@@ -4,19 +4,20 @@ import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.ContentType.URLENC
 import org.springframework.beans.factory.InitializingBean
 
-
 class MailchimpSTSService implements InitializingBean {
 
-	boolean transactional = false
-    def grailsApplication
-	def http
-	String apikey
-	
+	static transactional = false
+
+	def grailsApplication
+
+	private HTTPBuilder http
+	private String apikey
+
 	void afterPropertiesSet() {
 		http = new HTTPBuilder( grailsApplication.config.gchimp.stsUrl )
 		apikey = grailsApplication.config.gchimp.apiKey
 	}
-	
+
 	//
 	//	Helper method to reduce some of the code duplication
 	//
@@ -26,7 +27,7 @@ class MailchimpSTSService implements InitializingBean {
 		http.handler.failure = { res, json ->
     		println "STS error: HTTP ${res.statusLine} ${json}"
 		}
-		
+
 		http.post(
 			path : path,
 			body: body,
@@ -36,7 +37,7 @@ class MailchimpSTSService implements InitializingBean {
 			callback( res, json )
 		}
 	}
-	
+
 	/*
 		API docs: http://apidocs.mailchimp.com/sts/1.0/sendemail.func.php
 		curl -l http://us1.sts.mailchimp.com/1.0/SendEmail \
@@ -52,8 +53,8 @@ class MailchimpSTSService implements InitializingBean {
 		     -d tags[0]=WelcomeEmail
 	*/
 	def send( message, callback ){
-		
-		def i = 0
+
+		int i = 0
 		def body = [
 			track_opens : message.track_opens instanceof Boolean ? message.track_opens : true,
 			track_clicks : message.track_clicks instanceof Boolean ? message.track_clicks : true,
@@ -63,19 +64,19 @@ class MailchimpSTSService implements InitializingBean {
 			'message[from_name]' : message.from_name,
 			'message[subject]' : message.subject
 		]
-		
-		message.to.split(",").each{ email -> 
+
+		message.to.split(",").each{ email ->
 			body["message[to_email][${i++}]"] = email
 		}
-		
+
 		i = 0
-		message.tags?.split(",")?.each{ tag -> 
+		message.tags?.split(",")?.each{ tag ->
 			body["tags[${i++}]"] = tag
 		}
-		
+
 		call( 'SendEmail', body, callback )
 	}
-	
+
 	/*
 		API docs: http://apidocs.mailchimp.com/sts/1.0/listverifiedemailaddresses.func.php
 		curl -l http://us1.sts.mailchimp.com/1.0/ListVerifiedEmailAddresses \
@@ -84,7 +85,7 @@ class MailchimpSTSService implements InitializingBean {
 	def listVerified( callback ){
 		call( 'ListVerifiedEmailAddresses', [:], callback )
 	}
-	
+
 	/*
 		API docs: http://apidocs.mailchimp.com/sts/1.0/deleteverifiedemailaddress.func.php
 		curl -l http://us1.sts.mailchimp.com/1.0/DeleteVerifiedEmailAddress \
@@ -94,7 +95,7 @@ class MailchimpSTSService implements InitializingBean {
 	def deleteVerified( email, callback ){
 		call( 'DeleteVerifiedEmailAddress', [ email : email ], callback )
 	}
-	
+
 	/*
 		API docs: http://apidocs.mailchimp.com/sts/1.0/verifyemailaddress.func.php
 		curl -l http://us1.sts.mailchimp.com/1.0/VerifyEmailAddress \
@@ -104,7 +105,7 @@ class MailchimpSTSService implements InitializingBean {
 	def verify( email, callback ){
 		call( 'VerifyEmailAddress', [ email : email ], callback )
 	}
-	
+
 	/*
 		API docs: http://apidocs.mailchimp.com/sts/1.0/getbounces.func.php
 	*/
@@ -112,7 +113,7 @@ class MailchimpSTSService implements InitializingBean {
 		// since is a string in "Y-m-d H:i:s" format
 		call( 'GetBounces', [since:since], callback )
 	}
-	
+
 	/*
 		API docs: http://apidocs.mailchimp.com/sts/1.0/getsendstats.func.php
 	*/
@@ -120,7 +121,7 @@ class MailchimpSTSService implements InitializingBean {
 		// since is a string in "Y-m-d H:i:s" format
 		call( 'GetSendStats', [tag_id:tag_id,since:since], callback )
 	}
-	
+
 	/*
 		API docs: http://apidocs.mailchimp.com/sts/1.0/gettags.func.php
 		curl -l http://us1.sts.mailchimp.com/1.0/GetTags \
@@ -129,7 +130,7 @@ class MailchimpSTSService implements InitializingBean {
 	def getTags( callback ){
 		call( 'GetTags', [:], callback )
 	}
-	
+
 	/*
 		API docs: http://apidocs.mailchimp.com/sts/1.0/geturlstats.func.php
 	*/
@@ -138,7 +139,7 @@ class MailchimpSTSService implements InitializingBean {
 		// url_id - is an int referencing the URL record
 		call( 'GetUrlStats', [url_id:url_id,since:since], callback )
 	}
-	
+
 	/*
 		API docs: http://apidocs.mailchimp.com/sts/1.0/geturls.func.php
 		curl -l http://us1.sts.mailchimp.com/1.0/GetTags \
@@ -147,7 +148,7 @@ class MailchimpSTSService implements InitializingBean {
 	def getUrls( callback ){
 		call( 'GetUrls', [:], callback )
 	}
-	
+
 	/*
 		API docs: http://apidocs.mailchimp.com/sts/1.0/getsendquota.func.php
 		curl -l http://us1.sts.mailchimp.com/1.0/GetSendQuota \
@@ -156,7 +157,7 @@ class MailchimpSTSService implements InitializingBean {
 	def getSendQuota( callback ){
 		call( 'GetSendQuota', [:], callback )
 	}
-	
+
 	/*
 		API docs: http://apidocs.mailchimp.com/sts/1.0/getsendstatistics.func.php
 		curl -l http://us1.sts.mailchimp.com/1.0/GetSendStatistics \
@@ -166,4 +167,3 @@ class MailchimpSTSService implements InitializingBean {
 		call( 'GetSendStatistics', [:], callback )
 	}
 }
-
